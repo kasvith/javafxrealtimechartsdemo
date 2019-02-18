5850eeda2c3a78eb72706a1a312f7bb66d949a0f
Kasun Vithanage <alankasun@gmail.com>
2019-02-18 11:38:52 +0530

    initial

---
+++ src/main/java/App.java
@@ -0,0 +1,71 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class App extends Application {
+    private ScheduledExecutorService scheduledExecutorService;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX Realtime Chart Demo");
+
+        //defining the axes
+        final CategoryAxis xAxis = new CategoryAxis(); // we are gonna plot against time
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Time/s");
+        xAxis.setAnimated(false); // axis animations are removed
+        yAxis.setLabel("Value");
+        yAxis.setAnimated(false); // axis animations are removed
+
+        //creating the line chart with two axis created above
+        final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Realtime JavaFX Charts");
+        lineChart.setAnimated(false); // disable animations
+
+        //defining a series to display data
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Data Series");
+
+        lineChart.getData().add(series);
+
+        // setup scene
+        Scene scene = new Scene(lineChart, 800, 600);
+        primaryStage.setScene(scene);
+
+        // show the stage
+        primaryStage.show();
+
+        // setup a thread to add new data to the chart
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            Integer random = ThreadLocalRandom.current().nextInt(10);
+            Platform.runLater(() -> {
+                Date now = new Date();
+                series.getData().add(new XYChart.Data<>(simpleDateFormat.format(now), random));
+            });
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        scheduledExecutorService.shutdownNow();
+    }
+}
