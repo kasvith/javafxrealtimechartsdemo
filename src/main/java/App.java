e2638d04a87546c0c5734c0baef82aacf6765bff
Kasun Vithanage <alankasun@gmail.com>
2019-12-07 19:43:36 +0530

    update to latest java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,9 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
@@ -7,79 +13,72 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
-
 public class App extends Application {
-    final int WINDOW_SIZE = 10;
-    private ScheduledExecutorService scheduledExecutorService;
+  final int WINDOW_SIZE = 10;
+  private ScheduledExecutorService scheduledExecutorService;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) { launch(args); }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("JavaFX Realtime Chart Demo");
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setTitle("JavaFX Realtime Chart Demo");
 
-        //defining the axes
-        final CategoryAxis xAxis = new CategoryAxis(); // we are gonna plot against time
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Time/s");
-        xAxis.setAnimated(false); // axis animations are removed
-        yAxis.setLabel("Value");
-        yAxis.setAnimated(false); // axis animations are removed
+    // defining the axes
+    final CategoryAxis xAxis =
+        new CategoryAxis(); // we are gonna plot against time
+    final NumberAxis yAxis = new NumberAxis();
+    xAxis.setLabel("Time/s");
+    xAxis.setAnimated(false); // axis animations are removed
+    yAxis.setLabel("Value");
+    yAxis.setAnimated(false); // axis animations are removed
 
-        //creating the line chart with two axis created above
-        final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
-        lineChart.setTitle("Realtime JavaFX Charts");
-        lineChart.setAnimated(false); // disable animations
+    // creating the line chart with two axis created above
+    final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
+    lineChart.setTitle("Realtime JavaFX Charts");
+    lineChart.setAnimated(false); // disable animations
 
-        //defining a series to display data
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
-        series.setName("Data Series");
+    // defining a series to display data
+    XYChart.Series<String, Number> series = new XYChart.Series<>();
+    series.setName("Data Series");
 
-        // add series to chart
-        lineChart.getData().add(series);
+    // add series to chart
+    lineChart.getData().add(series);
 
-        // setup scene
-        Scene scene = new Scene(lineChart, 800, 600);
-        primaryStage.setScene(scene);
+    // setup scene
+    Scene scene = new Scene(lineChart, 800, 600);
+    primaryStage.setScene(scene);
 
-        // show the stage
-        primaryStage.show();
+    // show the stage
+    primaryStage.show();
 
-        // this is used to display time in HH:mm:ss format
-        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+    // this is used to display time in HH:mm:ss format
+    final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
 
-        // setup a scheduled executor to periodically put data into the chart
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    // setup a scheduled executor to periodically put data into the chart
+    scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
-        // put dummy data onto graph per second
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
-            // get a random integer between 0-10
-            Integer random = ThreadLocalRandom.current().nextInt(10);
+    // put dummy data onto graph per second
+    scheduledExecutorService.scheduleAtFixedRate(() -> {
+      // get a random integer between 0-10
+      Integer random = ThreadLocalRandom.current().nextInt(10);
 
-            // Update the chart
-            Platform.runLater(() -> {
-                // get current time
-                Date now = new Date();
-                // put random number with current time
-                series.getData().add(new XYChart.Data<>(simpleDateFormat.format(now), random));
+      // Update the chart
+      Platform.runLater(() -> {
+        // get current time
+        Date now = new Date();
+        // put random number with current time
+        series.getData().add(
+            new XYChart.Data<>(simpleDateFormat.format(now), random));
 
-                if (series.getData().size() > WINDOW_SIZE)
-                    series.getData().remove(0);
-            });
-        }, 0, 1, TimeUnit.SECONDS);
-    }
+        if (series.getData().size() > WINDOW_SIZE)
+          series.getData().remove(0);
+      });
+    }, 0, 100, TimeUnit.MILLISECONDS);
+  }
 
-    @Override
-    public void stop() throws Exception {
-        super.stop();
-        scheduledExecutorService.shutdownNow();
-    }
+  @Override
+  public void stop() throws Exception {
+    super.stop();
+    scheduledExecutorService.shutdownNow();
+  }
 }
